efe04c7fb19e4c098a8d2b17dfcf875a16842797
Farhan <farhantahir3@gmail.com>
2020-04-16 22:11:43 +0530

    Updates

---
+++ precache-manifest.108343c7bbeebd0d2925d3d95fcd3f0b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f06d17586f2b74860a6165359fd887b5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "85abe0fdecf088477f8a",
+    "url": "/static/css/main.5327e2ff.chunk.css"
+  },
+  {
+    "revision": "cbc7cc9f14c00875d6ed",
+    "url": "/static/js/2.a41fc77e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a41fc77e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85abe0fdecf088477f8a",
+    "url": "/static/js/main.f2d73ef0.chunk.js"
+  },
+  {
+    "revision": "7d07ac724fdff6b35275",
+    "url": "/static/js/runtime-main.dbd2aaf9.js"
+  },
+  {
+    "revision": "6844f8debeaebf7e70db1bda21cc661d",
+    "url": "/static/media/newYorkTimesLogoPngTransparent.6844f8de.png"
+  },
+  {
+    "revision": "bbefc0547c70b5656f3a86526c1e747f",
+    "url": "/static/media/treasure.bbefc054.png"
+  }
+]);
